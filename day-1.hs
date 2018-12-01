52fa40a05795fdb61dd3711066bbbe466f1ed560
Martín Valdés de León <m.valdesdeleon@sovanta.com>
2018-12-01 14:27:41 +0100

    Day 1, Part 2.

--- day-1.hs
+++ day-1.hs
@@ -1,4 +1,6 @@
 -- Day 1
+import Control.Monad.State
+import Data.Set (Set, empty, insert, member)
 
 loadInput = readFile "inputs/day-1a.txt"
 
@@ -7,14 +9,40 @@ fixPositives num@(n:ns) = if n == '+' then ns else num
 parse :: String -> [Integer]
 parse = map (read . fixPositives) . lines
 
-frequency :: [Integer] -> Integer
+type Freq = Integer
+
+frequency :: [Integer] -> Freq
 frequency = sum
 
-firstDupeFreq :: [Integer] -> Integer
-firstDupeFreq = undefined
+firstDupeFreq :: [Integer] -> Freq
+firstDupeFreq ns = evalState (loop ns) (empty, 0)
+
+type Freqs = Set Freq
+type FreqsState = State (Freqs, Freq)
+
+updateFreqState :: Freq -> (Freqs, Freq) -> (Freqs, Freq)
+updateFreqState freq (freqs, _) = (insert freq freqs, freq)
+
+step :: Integer -> FreqsState (Maybe Freq)
+step n = do
+    (freqs, lastFreq) <- get
+    let nextFreq = lastFreq + n
+    if nextFreq `member` freqs then
+        return $ Just nextFreq
+    else do
+        modify $ updateFreqState nextFreq
+        return Nothing
+
+loop :: [Integer] -> FreqsState Freq
+loop [] = error "No results"
+loop (n:ns) = do
+    res <- step n
+    case res of
+        Just freq -> return freq
+        Nothing -> loop ns
 
 main :: IO ()
 main = do
     input <- parse <$> loadInput
     print $ frequency input
-    print $ firstDupeFreq input
+    print $ firstDupeFreq (cycle input)
