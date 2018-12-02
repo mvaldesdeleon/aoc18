cf5d293f8c5a89c22bd4ae58fc54dd762965a0df
Martín Valdés de León <m.valdesdeleon@sovanta.com>
2018-12-02 11:47:00 +0100

    Day 2, Part 2.

--- day-2.hs
+++ day-2.hs
@@ -20,7 +20,33 @@ mult = uncurry (*)
 checksum :: [String] -> Integer
 checksum = mult . foldl addParams (0, 0) . map checksumParams
 
+consecutiveDupe :: Ord a => [a] -> Maybe a
+consecutiveDupe (a:b:cs) = if a == b then Just a else consecutiveDupe (b:cs)
+consecutiveDupe _ = Nothing
+
+dropNth :: Integer -> [a] -> [a]
+dropNth _ [] = []
+dropNth n (a:as)
+    | n < 0 = (a:as)
+    | n == 0 = as
+    | otherwise = a : dropNth (n - 1) as
+
+idSize :: [ID] -> Integer
+idSize = toInteger . length . head
+
+testPosition :: Integer -> [ID] -> Maybe ID
+testPosition n = consecutiveDupe . sort . map (dropNth (n - 1))
+
+findCommon :: [ID] -> ID
+findCommon ids = findFrom max
+    where max = idSize ids
+          findFrom 0 = error "No results"
+          findFrom n = case testPosition n ids of
+            Just id -> id
+            Nothing -> findFrom (n - 1)
+
 main :: IO ()
 main = do
     input <- parse <$> loadInput
     print $ checksum input
+    print $ findCommon input
