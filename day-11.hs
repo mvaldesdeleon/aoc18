e77fce6842ff4b0d1f523752c5e94ca4881582e3
Martín Valdés de León <m.valdesdeleon@sovanta.com>
2018-12-11 23:47:25 +0100

    Use Map.

--- day-11.hs
+++ day-11.hs
@@ -1,8 +1,9 @@
-import           Data.Char      (digitToInt)
+import           Data.Char       (digitToInt)
 import           Data.Function
 import           Data.List
-import qualified Data.List.Safe as L
-import           Data.Maybe     (fromMaybe)
+import qualified Data.List.Safe  as L
+import qualified Data.Map.Strict as M
+import           Data.Maybe      (fromMaybe)
 
 loadInput :: IO String
 loadInput = readFile "inputs/day-11.txt"
@@ -43,15 +44,19 @@ maxTotalPower gridSerial = snd . maximumBy (compare `on` fst) $ options
         , x <- [1 .. 301 - z]
         , y <- [1 .. 301 - z]
         ]
-    grid = [[powerLevel gridSerial (x, y) | y <- [1 .. 300]] | x <- [1 .. 300]]
-    sumRows = map (scanl (+) 0)
-    sumColumns = scanl (zipWith (+)) (replicate 301 0)
-    sumGrid = sumColumns . sumRows $ grid
+    get = M.findWithDefault 0
+    sumGrid =
+        foldl buildSums M.empty [(x, y) | y <- [1 .. 300], x <- [1 .. 300]]
+    buildSums m (x, y) =
+        M.insert
+            (x, y)
+            (powerLevel gridSerial (x, y) + get (x - 1, y) m + get (x, y - 1) m -
+             get (x - 1, y - 1) m)
+            m
     totalPower z (x, y) =
-        ( (sumGrid !! (x + z - 1) !! (y + z - 1)) -
-          (sumGrid !! (x - 1) !! (y + z - 1)) -
-          (sumGrid !! (x + z - 1) !! (y - 1)) +
-          (sumGrid !! (x - 1) !! (y - 1))
+        ( get (x + z - 1, y + z - 1) sumGrid - get (x - 1, y + z - 1) sumGrid -
+          get (x + z - 1, y - 1) sumGrid +
+          get (x - 1, y - 1) sumGrid
         , (fromIntegral x, fromIntegral y, fromIntegral z))
 
 main :: IO ()
