c67bd7f75fcded2be01b7886f7c6734e3e9036e4
Martín Valdés de León <m.valdesdeleon@sovanta.com>
2018-12-15 01:01:31 +0100

    Day 14, Part 2.

--- day-14.hs
+++ day-14.hs
@@ -1,9 +1,10 @@
 {-# LANGUAGE RecordWildCards #-}
 
 import           Control.Monad       (replicateM_)
-import           Control.Monad.Loops (iterateUntil)
+import           Control.Monad.Loops (iterateUntil, untilM_)
 import           Control.Monad.State (State, execState, gets, modify)
 import           Data.Char           (digitToInt, intToDigit)
+import           Data.List           (genericLength)
 import qualified Data.Map.Strict     as M
 
 loadInput :: IO String
@@ -52,19 +53,48 @@ advance i scores = (1 + i + scores M.! i) `mod` fromIntegral (M.size scores)
 digits :: Integer -> [Integer]
 digits = map (fromIntegral . digitToInt) . show
 
-brewUntil :: Integer -> State Recipes Integer
-brewUntil count = iterateUntil (>= count) make
+brewMany :: Integer -> State Recipes Integer
+brewMany count = iterateUntil (>= count) make
 
 tenScores :: Integer -> String
 tenScores input = sumSeries 10 input scores
   where
-    scores = rScores $ execState (brewUntil $ input + 10) initial
+    scores = rScores $ execState (brewMany $ input + 10) initial
 
 sumSeries :: Integer -> Integer -> Scores -> String
 sumSeries count i scores =
     map (intToDigit . fromIntegral . (scores M.!)) [i .. i + count - 1]
 
+brewSequence :: Integer -> State Recipes ()
+brewSequence seq = make `untilM_` contains seq
+
+contains :: Integer -> State Recipes Bool
+contains seq = do
+    scores <- gets rScores
+    let s = show seq
+    let l = genericLength s
+    let i = fromIntegral (M.size scores) - l
+    return $
+        (i >= 0 && sumSeries l i scores == s) ||
+        (i >= 1 && sumSeries l (i - 1) scores == s)
+
+toTheLeft :: Integer -> Integer
+toTheLeft input = find input scores
+  where
+    scores = rScores $ execState (brewSequence input) initial
+
+find :: Integer -> Scores -> Integer
+find seq scores =
+    if sumSeries l i scores == s
+        then i
+        else i - 1
+  where
+    s = show seq
+    l = genericLength s
+    i = fromIntegral (M.size scores) - l
+
 main :: IO ()
 main = do
     input <- parseInput <$> loadInput
     print $ tenScores input
+    print $ toTheLeft input
