e94af8413d3f3ecdae7bca78c5dda0f63463d0a8
Martín Valdés de León <m.valdesdeleon@sovanta.com>
2018-12-23 15:32:10 +0100

    Day 23, Part 2.

--- day-23.hs
+++ day-23.hs
@@ -3,6 +3,7 @@
 import           Control.Lens
 import           Data.Foldable  (length)
 import           Data.Function  (on)
+import           Data.List      (foldl1', genericLength)
 import qualified Data.List.Safe as L
 import           Text.Parsec    (Parsec, between, char, digit, many1, newline,
                                  option, parse, sepBy, string)
@@ -21,6 +22,9 @@ makeLenses ''Position
 distance :: Position -> Position -> Integer
 distance pa pb =
     abs (pb ^. x - pa ^. x) + abs (pb ^. y - pa ^. y) + abs (pb ^. z - pa ^. z)
+
+origin :: Position
+origin = Position 0 0 0
 
 data NanoBot = NanoBot
     { _pos :: Position
@@ -56,8 +60,29 @@ parseInput input =
         Left e   -> error $ show e
         Right ns -> ns
 
+improve :: [NanoBot] -> Position -> Integer -> Position
+improve ns (Position ox oy oz) res = fst $ foldl1' best cs
+  where
+    size = 20
+    step = 10 ^ res
+    ps =
+        [ Position (ox + x * step) (oy + y * step) (oz + z * step)
+        | z <- [-size .. size]
+        , y <- [-size .. size]
+        , x <- [-size .. size]
+        ]
+    cs = (\p -> (p, count p)) <$> ps
+    count p = genericLength . filter (`inRangeOf` p) $ ns
+    best (pa, ca) (pb, cb)
+        | ca > cb = (pa, ca)
+        | cb > ca = (pb, cb)
+        | distance pa origin < distance pb origin = (pa, ca)
+        | otherwise = (pb, cb)
+
 main :: IO ()
 main = do
     input <- parseInput <$> loadInput
     let Just st = strongest input
     print $ length . filter (inRangeOf st . view pos) $ input
+    print $
+        distance origin $ foldl (improve input) origin [7, 6, 5, 4, 3, 2, 1, 0]
